871ac1f889d3578cbd702efa9dbb7d9a1b4d02c7
Dave Winniford <dwinniford@gmail.com>
2020-11-20 10:08:13 -0600

    Done

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 function bfs(rootNode, vertices, edges){
+    const orderedNodes = []
+    let queued = []
+    rootNode.distance = 0
+    queued.push(rootNode)
+    while(queued.length !== 0 ) {
+        const currentNode = queued.shift()
+        console.log(currentNode)
+        const adjacentNodes = findAdjacentNodes(currentNode.name, vertices, edges)
+        console.log("adjacentNodes=", adjacentNodes)
 
+        markDistanceAndPredecessor(currentNode, adjacentNodes)
+        
+        queued = queued.concat(adjacentNodes)
+        orderedNodes.push(currentNode)
+    };
+    return orderedNodes
+}
+
+function findAdjacentNodes(rootNode, vertices, edges) {
+    const currentNode = vertices.find(v => v.name === rootNode)
+    const adjacentNodes = []
+    for(const edge of edges) {
+        if(edge.includes(rootNode)) {
+            for(const vName of edge) {
+                if(vName !== rootNode) {
+                    for(const vertice of vertices) {
+                        if(vertice.name === vName && vertice.distance === null) {
+                            adjacentNodes.push(vertice)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return adjacentNodes
+}
+
+function markDistanceAndPredecessor(rootNode, adjacentNodes) {
+    for(const n of adjacentNodes) {
+        n.distance = rootNode.distance + 1
+        n.predecessor = rootNode
+    }
+    return adjacentNodes
 }
